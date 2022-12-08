08b24ed263b67338aeafe066942627cc44882111
wilsennn <wilsenfendrius@mhs.mdp.ac.id>
2022-12-08 13:05:25 +0700

    update
    8/12/22

--- app/src/main/java/com/si5b/justquote/Activity/MainActivity.java
+++ app/src/main/java/com/si5b/justquote/Activity/MainActivity.java
@@ -1,16 +1,24 @@
 package com.si5b.justquote.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.ProgressBar;
 
 import com.si5b.justquote.R;
 
 public class MainActivity extends AppCompatActivity {
-
+    private RecyclerView rvQuote;
+    private ProgressBar pbQuote;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rvQuote = findViewById(R.id.rv_quote);
+        pbQuote = findViewById(R.id.pb_quote);
+        rvQuote.setLayoutManager(new LinearLayoutManager(this));
     }
 }
